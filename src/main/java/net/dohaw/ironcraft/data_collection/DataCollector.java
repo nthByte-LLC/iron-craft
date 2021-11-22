85d28793064583db25cd3693f58f3830ea1f8d1f
Caleb Owens <caleb.ja.owens@gmail.com>
2021-11-22 13:09:16 -0500

    Cleaned up DataCollector.

--- src/main/java/net/dohaw/ironcraft/data_collection/DataCollector.java
+++ src/main/java/net/dohaw/ironcraft/data_collection/DataCollector.java
@@ -1,13 +1,15 @@
 package net.dohaw.ironcraft.data_collection;
 
 import net.dohaw.ironcraft.IronCraftPlugin;
-import org.bukkit.Bukkit;
+import net.dohaw.ironcraft.playerdata.PlayerData;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.scheduler.BukkitRunnable;
 
-import java.util.*;
+import java.util.Locale;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class DataCollector extends BukkitRunnable {
 
@@ -17,23 +19,20 @@ public class DataCollector extends BukkitRunnable {
         this.ironCraftPlugin = ironCraftPlugin;
     }
 
-
     @Override
     public void run() {
 
         // loop through all the player uuids that have been assigned a chamber
-        for (UUID uuid : this.ironCraftPlugin.getPlayerDataHandler().getAllPlayerData().keySet()) {
+        for (PlayerData data : this.ironCraftPlugin.getPlayerDataHandler().getAllPlayerData().values()) {
 
             // get the player
-            Player player = Bukkit.getPlayer(UUID.fromString(uuid.toString()));
-
+            Player player = data.getPlayer();
             if (player == null) {
                 continue;
             }
 
             // get the player's inventory
             Inventory inventory = player.getInventory();
-
             // loop through the player's inventory
             for (ItemStack itemStack : inventory.getContents()) {
 
@@ -46,8 +45,10 @@ public class DataCollector extends BukkitRunnable {
                 // store the item's name and amount in inventoryData
                 inventoryData.put(itemStack.getType().toString().toLowerCase(Locale.ROOT), itemStack.getAmount());
                 // add the inventoryData to the list in the PlayerData object.
-                this.ironCraftPlugin.getPlayerDataHandler().getData(uuid).addInventoryData(inventoryData);
+                data.addInventoryData(inventoryData);
+
             }
         }
+
     }
 }
